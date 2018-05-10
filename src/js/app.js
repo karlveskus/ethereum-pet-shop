188d957cd99891c6d6d078f27c0c052b8889246c
Karl Veskus <karl.veskus@gmail.com>
2018-05-10 20:48:23 +0300

    Add .gitignore

--- src/js/app.js
+++ src/js/app.js
@@ -24,15 +24,14 @@ App = {
   },
 
   initWeb3: function() {
-    // Initialize web3 and set the provider to the testRPC.
+    // Is there an injected web3 instance?
     if (typeof web3 !== 'undefined') {
       App.web3Provider = web3.currentProvider;
-      web3 = new Web3(web3.currentProvider);
     } else {
-      // set the provider you want from Web3.providers
-      App.web3Provider = new web3.providers.HttpProvider('http://localhost:8545');
-      web3 = new Web3(App.web3Provider);
+      // If no injected web3 instance is detected, fall back to Ganache
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
     }
+    web3 = new Web3(App.web3Provider);
 
     return App.initContract();
   },
@@ -109,3 +108,6 @@ $(function() {
     App.init();
   });
 });
+
+
+
